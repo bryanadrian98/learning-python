be8f4cd17e39380cbe784411e832f8670cec3372
Bryan Adrian <bryanadrian1998@gmail.com>
2026-03-26 02:53:04 +0700

    Day 1: variables and data types

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Day 1") 
+# Variables and data types
+
+name = "Bryan"
+age = 27
+height = 175.5
+is_employed = True
+
+print(name)
+print(age)
+print(height)
+print(is_employed)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_employed))
+print(age + height)
