74ce874a8eb2e5c9986f43ce573bbc2441978a3e
Bryan Adrian <bryanadrian1998@gmail.com>
2026-03-27 03:53:24 +0700

    revision

--- month-01/exercises/calculator.py
+++ month-01/exercises/calculator.py
@@ -1,16 +1,3 @@
-num1 = int(input("Please input the first number here: "))
-num2 = int(input("Please input the second number here: "))
-
-# print(f"Sum: {num1 + num2}")
-# print(f"Subtraction: {num1 - num2}")
-# print(f"Multiplication: {num1 * num2}")
-# print(f"Division: {num1 / num2}")
-# print(f"Remainder: {num1 % num2}")
-# print(f"Power: {num1 ** num2}")
-# print(f"Floor Division: {num1 // num2}")
-
-# print(-7 // 2)   # what do you expect?
-
 def add(a, b):
     return a + b
 
@@ -26,11 +13,14 @@ def division(a, b):
 def mod(a, b):
     return a % b
 
-def div(a, b):
+def floor_division(a, b):
     return a // b
 
 def power(a, b):
     return a ** b
+
+num1 = int(input("Please input the first number here: "))
+num2 = int(input("Please input the second number here: "))
 
 print(f"Sum: {add(num1, num2)}")
 print(f"Subtraction: {subtract(num1, num2)}")
@@ -38,4 +28,4 @@ print(f"Multiplication: {multi(num1, num2)}")
 print(f"Division: {division(num1, num2)}")
 print(f"Remainder: {mod(num1, num2)}")
 print(f"Power: {power(num1, num2)}")
-print(f"Floor Division: {div(num1, num2)}")
+print(f"Floor Division: {floor_division(num1, num2)}")
--- month-01/exercises/functions_practice.py
+++ month-01/exercises/functions_practice.py
@@ -1,3 +1,21 @@
+# Function 1: Temperature Checker
+def celcius_to_fahrenheit(celcius):
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit
+
+# Function 2: Palindrome checker
+def palindrome_checker(word):
+    result = word == word[::-1]
+    return result
+
+# Function 3: Vower counter
+def count_vowels(sentence):
+    vowels = 0
+    for x in sentence.lower():
+        if x in ["a", "i", "u", "e", "o"]:
+            vowels +=1
+    return vowels
+
 print("Please choose which fucntion you want to use.")
 print("1. Celcius to Fahrenheit")
 print("2. Check Palindrome")
@@ -6,36 +24,15 @@ print("3. Count Vowels")
 choice = str(input("Enter 1, 2, or 3: "))
 
 if choice == "1":
-    # Function 1: Temperature Checker
     celcius = float(input("Please input Celcius: "))
-
-    def celcius_to_fahrenheit(celcius):
-        fahrenheit = (celcius * 9/5) * 32
-        return fahrenheit
-
     print(f"So {celcius} degrees Celcius equal to {celcius_to_fahrenheit(celcius)} degrees Fahrenheit.")
 
 elif choice == "2":
-    # Function 2: Palindrome checker
     word = str(input("Input your word here: "))
-
-    def palindrome_checker(word):
-        result = word == word[::-1]
-        return result
-
     print(f"Palindrome checker result: {palindrome_checker(word)}")
 
 elif choice == "3":
-    # Function 3: Vower counter
     sentence = str(input("Input your sentence here: "))
-
-    def count_vowels(sentence):
-        vowels = 0
-        for x in sentence.lower():
-            if x in ["a", "i", "u", "e", "o"]:
-                vowels +=1
-        return vowels
-
     print(f"The number of vowels in this setence is: {count_vowels(sentence)}.")
 
 else:
