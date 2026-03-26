22467ba243fd3b4d3d4fc0cb06ffb5703df8734e
Bryan Adrian <bryanadrian1998@gmail.com>
2026-03-27 01:35:52 +0700

    revision

--- month-01/exercises/calculator.py
+++ month-01/exercises/calculator.py
@@ -2,8 +2,10 @@ num1 = int(input("Please input the first number here: "))
 num2 = int(input("Please input the second number here: "))
 
 print(f"Sum: {num1 + num2}")
-print(f"Substraction: {num1 - num2}")
+print(f"Subtraction: {num1 - num2}")
 print(f"Multiplication: {num1 * num2}")
 print(f"Division: {num1 / num2}")
 print(f"Remainder: {num1 % num2}")
 print(f"Power: {num1 ** num2}")
+
+print(-7 // 2)   # what do you expect?
